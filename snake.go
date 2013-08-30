1ce38f2b35f625e7a53e4c06b1117d93cf9e760f
hSATAC <hsatac@gmail.com>
2013-08-30 16:40:19 +0800

    Implement basic snake move

--- snake.go
+++ snake.go
@@ -17,15 +17,41 @@ type Node struct {
 	y int
 }
 
+func (snake *Snake) Move() {
+	// TODO:if eat fruit, don't kill tail here.
+	snake.Body = snake.Body[1:]
+
+	head := snake.head()
+	var new_head Node
+
+	// TODO: deal over edge case here.
+	switch snake.Direction {
+	case SNAKE_DIRECTION_RIGHT:
+		new_head = Node{x: head.x + 1, y: head.y}
+	case SNAKE_DIRECTION_DOWN:
+		new_head = Node{x: head.x, y: head.y + 1}
+	case SNAKE_DIRECTION_LEFT:
+		new_head = Node{x: head.x - 1, y: head.y}
+	case SNAKE_DIRECTION_UP:
+		new_head = Node{x: head.x, y: head.y - 1}
+	}
+
+	snake.Body = append(snake.Body, new_head)
+}
+
+func (snake *Snake) Len() int {
+	return len(snake.Body)
+}
+
+func (snake *Snake) head() Node {
+	return snake.Body[snake.Len()-1]
+}
+
 func NewSnake() *Snake {
 	snake := Snake{}
 
 	// give default
 	snake.Direction = SNAKE_DIRECTION_RIGHT
-	snake.Body = append(snake.Body, Node{x: 0, y: 0})
+	snake.Body = []Node{Node{x: 0, y: 0}, Node{x: 1, y: 0}}
 	return &snake
-}
-
-func (snake *Snake) Len() int {
-	return len(snake.Body)
 }
--- snake_test.go
+++ snake_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -8,11 +9,49 @@ import (
 func TestSnake(t *testing.T) {
 	var snake = NewSnake()
 	assert.Equal(t, snake.Direction, SNAKE_DIRECTION_RIGHT, "")
-	assert.Equal(t, len(snake.Body), 1, "")
+	assert.Equal(t, len(snake.Body), 2, "")
+	spew.Dump("test")
 }
 
 func TestSnakeLen(t *testing.T) {
 	var snake = NewSnake()
 	snake.Body = []Node{Node{x: 0, y: 0}, Node{x: 1, y: 1}}
 	assert.Equal(t, snake.Len(), 2, "")
+}
+
+func TestSnakeMove(t *testing.T) {
+	var snake = NewSnake()
+	snake.Move()
+	assert.Equal(t, snake.Len(), 2, "")
+	assert.Equal(t, snake.Body[0], Node{x: 1, y: 0})
+	assert.Equal(t, snake.Body[1], Node{x: 2, y: 0})
+}
+
+func TestSnakeMoveDown(t *testing.T) {
+	var snake = NewSnake()
+	snake.Direction = SNAKE_DIRECTION_DOWN
+	snake.Move()
+	assert.Equal(t, snake.Len(), 2, "")
+	assert.Equal(t, snake.Body[0], Node{x: 1, y: 0})
+	assert.Equal(t, snake.Body[1], Node{x: 1, y: 1})
+}
+
+func TestSnakeMoveLeft(t *testing.T) {
+	var snake = NewSnake()
+	snake.Direction = SNAKE_DIRECTION_LEFT
+	snake.Body = []Node{Node{x: 5, y: 5}, Node{x: 4, y: 5}}
+	snake.Move()
+	assert.Equal(t, snake.Len(), 2, "")
+	assert.Equal(t, snake.Body[0], Node{x: 4, y: 5})
+	assert.Equal(t, snake.Body[1], Node{x: 3, y: 5})
+}
+
+func TestSnakeMoveUp(t *testing.T) {
+	var snake = NewSnake()
+	snake.Direction = SNAKE_DIRECTION_UP
+	snake.Body = []Node{Node{x: 5, y: 5}, Node{x: 4, y: 5}}
+	snake.Move()
+	assert.Equal(t, snake.Len(), 2, "")
+	assert.Equal(t, snake.Body[0], Node{x: 4, y: 5})
+	assert.Equal(t, snake.Body[1], Node{x: 4, y: 4})
 }
